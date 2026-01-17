67d2ae6c91cc3ed7527e13f0df80d20822cb378d
Gardar Hauksson <gardarh@everenanalytics.com>
2026-01-18 00:26:29 +0100

    Music script: Pause music when set to source.

--- stofa_music.sh
+++ stofa_music.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 HOST="${SONOS_HOST:-localhost}"
 curl -X POST "http://${HOST}:5000/speakers/Stofa/set-source" -H "Content-Type: application/json" -d '{"source": "music"}'
+curl -X POST "http://${HOST}:5000/speakers/Stofa/pause"
 curl -X POST "http://${HOST}:5000/speakers/Stofa/volume" -H "Content-Type: application/json" -d '{"volume": 15}'
