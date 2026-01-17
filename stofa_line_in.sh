9d0e57976efd52d4936a684b50768f0333d02da1
Gardar Hauksson <gardarh@everenanalytics.com>
2026-01-17 23:52:52 +0100

    Update scripts to use SONOS_HOST env variable if present.

--- stofa_line_in.sh
+++ stofa_line_in.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-curl -X POST http://localhost:5000/speakers/Stofa/set-source -H "Content-Type: application/json" -d '{"source": "line-in"}'
-curl -X POST http://localhost:5000/speakers/Stofa/volume -H "Content-Type: application/json" -d '{"volume": 75}'
-curl -X POST http://localhost:5000/speakers/Stofa/play
+HOST="${SONOS_HOST:-localhost}"
+curl -X POST "http://${HOST}:5000/speakers/Stofa/set-source" -H "Content-Type: application/json" -d '{"source": "line-in"}'
+curl -X POST "http://${HOST}:5000/speakers/Stofa/volume" -H "Content-Type: application/json" -d '{"volume": 75}'
+curl -X POST "http://${HOST}:5000/speakers/Stofa/play"
--- stofa_music.sh
+++ stofa_music.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
-curl -X POST http://localhost:5000/speakers/Stofa/set-source -H "Content-Type: application/json" -d '{"source": "music"}'
-curl -X POST http://localhost:5000/speakers/Stofa/volume -H "Content-Type: application/json" -d '{"volume": 15}'
+HOST="${SONOS_HOST:-localhost}"
+curl -X POST "http://${HOST}:5000/speakers/Stofa/set-source" -H "Content-Type: application/json" -d '{"source": "music"}'
+curl -X POST "http://${HOST}:5000/speakers/Stofa/volume" -H "Content-Type: application/json" -d '{"volume": 15}'
